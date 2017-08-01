590053651dff32a1550f2aae2cdca94a829104af
lfenn <lfenn@ap.org>
2017-08-01 13:51:28 -0400

    Add in general switches for style development

---
+++ R/linechart_style.R
@@ -0,0 +1,36 @@
+#' Line chart in AP style
+#'
+#' This function presents an AP-styled line chart in various formats.
+#' @include theme_ap.R
+
+uk <- data.frame(Date = as.integer(12 * time(UKDriverDeaths))/12, UKDeaths = as.matrix(UKDriverDeaths))
+us <- data.frame(Date = as.integer(12 * time(USAccDeaths))/12, USDeaths = as.matrix(USAccDeaths))
+data <- merge(uk, us, by = "Date", all = TRUE)
+
+# Data style point: data to visuals should be tidy
+uk <- data[, c("Date", "UKDeaths")]
+us <- data[, c("Date", "USDeaths")]
+uk$Country <- "UK"
+us$Country <- "US"
+colnames(uk)[2] <- "Deaths"
+colnames(us)[2] <- "Deaths"
+data <- rbind(uk, us)[, c(1, 3, 2)]
+
+
+# Together ----------------------------------------------------------------
+
+# Q: Start at zero?
+# Q: Colors? (Consult Nathan's guide)
+
+# Code style:
+# Initial invocation
+p <- ggplot(data) +
+  # Plot objects
+  geom_line(aes(x = Date, y = Deaths, color = Country)) +
+  # Labels and legend
+  labs(title = "US & UK accident deaths") +
+  theme_ap()
+p
+# Faceted -----------------------------------------------------------------
+
+# p + facet_wrap(~Country, scales = 'free')
--- R/theme_ap.R
+++ R/theme_ap.R
@@ -3,6 +3,8 @@
 #' This function is a theme function for use with ggplot2.
 #' It tries to have a very tight composition, suitable for email or chat message
 #' @include aptheme-package.R
+#'
+#' http://ggplot2.tidyverse.org/reference/theme.html
 
 # primitives
 # element_text:
@@ -28,36 +30,61 @@
 
 
 #' @export
-theme_ap <- function() {
+theme_ap <- function(size = 12, family = "GoodComp-CondBook") {
   theme(
-    #  axis.line = element_blank(),
-    #  axis.text = element_text(),
-    #  axis.text.x = element_text(), # inheritance
-    #  axis.text.y = element_text(),
-    #  axis.title = element_text(),
-    #  axis.title.x = element_text(), # inheritance
-    #  axis.title.y = element_text(),
-    axis.ticks = element_blank(),
+    # Text elements
+    text = element_text(size, family = family),
+    title = element_text(size = size + 4),
+#    axis.text = element_blank(),
+#    axis.title = element_blank(),
+    # Question: x and y direction style difference? Should there be one? Maybe only for certain graphs (i.e. bar graphs)?
+#    legend.text = element_blank(),
+#    legend.title = element_blank(),
+    # Question: Plot title, axis title, legend title- all same, or different?
+#    plot.caption = element_blank(),
+#    plot.title = element_blank(),
+#    plot.subtitle = element_blank(),
+#    strip.text = element_blank(),
+
+    # Line elements
+#    line = element_blank(),
+#    axis.ticks = element_blank(),
+#    axis.line = element_blank(),
+#    panel.grid = element_blank(),
+    panel.grid.major = element_line(color='#E7E2D8'),
+    panel.grid.minor = element_line(color='#B6B6AB', linetype=1),
+
+    # Rect elements
+#    rect = element_blank(),
+#    legend.key = element_blank(),
+#    legend.background = element_blank(),
+#    legend.box.background = element_blank(),
+#    panel.background = element_blank(), # Drawn underneath the plot
+#    panel.border = element_blank(), # Drawn on top of plot, covering tick marks and grid lines (fill = NA)
+#    plot.background = element_blank(),
+#    strip.background = element_blank(),
+
+    # Other
     axis.ticks.length = unit(-2, "pt"),
-    #  panel.background = element_rect(fill = '#FFF9EE', linetype=0),
-    panel.background = element_blank(),
-    panel.grid.major = element_line(color='grey', linetype=1),
-    #  panel.grid.major.x = element_line(),
-    #  panel.grid.major.y = element_line(),
-    panel.grid.minor = element_line(color='lightgrey'),
-    #  panel.grid.minor.x = element_line(),
-    #  panel.grid.minor.y = element_line(),
-    #  plot.margin = unit(c(0, 0, 0, 0), "cm"),
-    plot.margin = margin(),
-    #  plot.background = element_blank(),
-    plot.title = element_text(face="bold"),
-    #  legend.box.background = element_rect(),
-    legend.key = element_blank(),
-    legend.position = "top",
-    legend.margin = margin(-15, 0, -15, 0),
-    aspect.ratio = 9/16
+
+    legend.margin = margin(-15, 0, -15, 0, "pt"),
+#    legend.spacing = element_blank(),
+#    legend.key.size = element_blank(),
+    legend.text.align = 0, # 0: left, 1: right
+    legend.title.align = 0,
+    legend.position = "right", # none, left, right, bottom, top, or two-element numeric vector
+    legend.direction = "vertical", # horizontal, vertical
+    legend.justification = "center", # center or two-element numeric vector
+    legend.box = "horizontal", # horizontal, vertical
+    legend.box.just = "left", # top, bottom, left, right
+    legend.box.margin = margin(0, 0, 0, 0, "pt"), # specified
+    legend.box.spacing = unit(0, "pt"),
+
+    panel.spacing = unit(0, "pt"),
+    plot.margin = margin(5, 0, 0, 0, "pt"),
+    strip.placement = "inside", # inside, outside
+    aspect.ratio = 3/4
   )}
 
 
-# TODO define a custom viz function that makes a scatterplot, accepts labels as a variable, and will automatically label the outliers
-
+# TODO: Define custom color scales (discrete, continuous) for use with AP
